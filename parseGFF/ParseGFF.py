d8f4eeadde6c293ed5f6e6185cf4282a847fe00c
Kathryn Judy <kjjudy@uark.edu>
2018-04-11 20:43:31 -0500

    attempts to make parseGFF modular (not complete)

--- parseGFF/ParseGFF.py
+++ parseGFF/ParseGFF.py
@@ -1,45 +1,70 @@
 #! /usr/bin/env python3
 
-#testing the branch process 2.0
-#columns 4-5 are start-end in .gff
+#currently only returns first "header" ... why?
+#issue probably within rev_comp_test
+
 #import modules
 import argparse
 from Bio import SeqIO
 
-#setup command-line argument input for module use with argparse
-#create ArgumentParser object to hold all info
-parser=argparse.ArgumentParser(description="This script writes out individual genes from a .fasta genome based on locations specified in a GenBank .gff file")
+#define functions
 
-#add arguments
-parser.add_argument("fasta",help="name of FASTA file, must be the first argument. Input the full path name if the file is not in current working directory.")
-parser.add_argument("gff",help="name of GFF file, must be the second argument. Input the full path name if file is not in current working directory.")
+#write function to take specific command-line input with argparse
+def get_args():
+	#create ArgumentParser object to hold all info
+	parser=argparse.ArgumentParser(description="This script writes out individual genes from a .fasta genome based on locations specified in a GenBank .gff file. The reverse complement of genes on the - strand are given.")
 
-#parse the arguments
-args=parser.parse_args()
+	#add arguments
+	parser.add_argument("fasta",help="name of FASTA file, must be the first argument. Input the full path name if the file is not in current working directory.")
+	parser.add_argument("gff",help="name of GFF file, must be the second argument. Input the full path name if file is not in current working directory.")
 
-#read in and store genome (.fasta) using SeqIO
-for genome in SeqIO.parse(args.fasta, "fasta"):
-   	#set variable
-	genome=genome.seq
+	#parse the arguments
+	return parser.parse_args()
 
-#open .gff file
-file=open(args.gff)
+#write function to parse GFF file
+#variables should be defined relative to other functions in main
+def gff_parse(f,g):
+	#read in and store genome (.fasta) using SeqIO
+	for genome in SeqIO.parse(f, "fasta"):
+   		#set variable
+		genome=genome.seq
 
-#give list input each line of .gff file
-for line in file:
+	#open .gff file
+	file=open(g)
 
-#split each line into lists of strings at the tab
-	list=line.split("\t")
-	attribute=list[8].split(";")
-	gene_info=attribute[0].split()
-#gene_name=gene_info[1], intron/exon=gene_info[2:3] but not always present-write as if/else to prevent code failure
+	#give list input each line of .gff file
+	for line in file:
+		#split each line into lists of strings at the tab
+		list=line.split("\t")
+		attribute=list[8].split(";")
+		gene_info=attribute[0].split()
+		#gene_name=gene_info[1], intron/exon=gene_info[2:3] but not always present-write as if/else to prevent code failure
 
-	#print(gene_info[1])
-	print(">"+list[0],gene_info[1])
-	#print(genome[(int(list[3])-1):(int(list[4])-1)])
-#print some "header info" (organism name, intron/exon, etc in .fasta style)
+		#provide FASTA style header- >organism gene (more later)
+		return ">"+list[0]+"_"+gene_info[1]
 
-#print data from genome at coordinates
+		#return nt sequence or reverse complement (uses following function)
+		return rev_comp_test(list,genome)
 
-#close .gff file
-file.close()
+		#close .gff file
+	file.close()
+
+#write function to calc reverse complement if needed
+def rev_comp_test(list,genome):
+#not fixed if list[6] is made into a variable, or if moved above prev
+	#determine if strand is + or -, calculate reverse complement of - strand nt
+	if list[6] == "+":
+		return genome[(int(list[3])-1):(int(list[4])-1)]
+	else:
+		rev_comp=reverse_complement(genome[(int(list[3])-1):(int(list[4])-1)])
+		return rev_comp
+
+#define main function (connects all the modular pieces)
+def main():
+	print(gff_parse(args.fasta,args.gff))
+
+#call command-line arguments
+args=get_args()
+
+#run main function
+main()
