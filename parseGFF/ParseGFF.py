857a88ad039d5a5b1cf9da60f7baa0b78222928a
Kathryn Judy <kjjudy@uark.edu>
2018-04-25 18:02:40 -0500

    edits to ParseGFF, jnhw7_7, adding jnhw8_8 and checkpoints

--- parseGFF/ParseGFF.py
+++ parseGFF/ParseGFF.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
 
-#currently only returns first "header" ... why?
-#issue probably within rev_comp_test
+#this script will parse a GFF file, extracting sequences for the annotated features
 
 #import modules
+import sys
+import os
+import re
 import argparse
 from Bio import SeqIO
 
@@ -21,49 +23,74 @@ def get_args():
 	#parse the arguments
 	return parser.parse_args()
 
+#write function to parse fasta file
+def fasta_parse(fasta):
+	#read in and return genome (.fasta) using SeqIO
+# for single-sequence fasta:
+	genome = SeqIO.read(fasta,"fasta")
+	return genome.seq
+#for multi-sequence fasta: (set as condition later)
+	#for genome in SeqIO.parse(fasta,"fasta"):
+		#return genome.seq
+
 #write function to parse GFF file
 #variables should be defined relative to other functions in main
 def gff_parse(f,g):
-	#read in and store genome (.fasta) using SeqIO
-	for genome in SeqIO.parse(f, "fasta"):
-   		#set variable
-		genome=genome.seq
 
 	#open .gff file
 	file=open(g)
 
 	#create lists for adding values
-	header=[]
-	body=[]
+	#header=[]
+	#body=[]
 
 	#give list input each line of .gff file
 	for line in file:
 		#split each line into lists of strings at the tab
-		list=line.split("\t")
-		attribute=list[8].split(";")
-		gene_info=attribute[0].split()
+		(seqid, source, feature, start, end, length, strand, frame, attributes)=line.split("\t")
+		#list=line.split("\t")
+		#attribute=attributes.split(" ; ")
+		#gene_info=attribute[0].split()
 		#gene_name=gene_info[1], intron/exon=gene_info[2:3] but not always present-write as if/else to prevent code failure
+		#pull out exons for further processing:
+		if(feature=="CDS" or feature=="tRNA" or feature=="rRNA"):
+			atts=attributes.split(" ; ")
+			gene_name=re.search("^Gene\s+(\S+)" , atts[0])
+			exon=re.search("exon\s+(\d+)")
+			if(gene_name and exon):
+				print(">"+seqid.replace(" ","_")+"_"+gene_name.group(1)+"_"exon.group(1))
+				#header.append(">"+seqid.replace(" ","_")+"_"+gene_name.group(1)+"_"exon.group(1))
+			else:
+				print(">"+seqid.replace(" ","_")+"_"+gene_name.group(1))
+				#header.append(">"+seqid.replace(" ","_")+"_"+gene_name.group(1))
 
-		header.append(">"+list[0]+"_"+gene_info[1])
-		body.append(rev_comp_test(list,genome))
+			if strand == "+":
+				print(f[(int(start)-1):(int(end)])
+
+			else:
+				print(rev_comp(genome[(int(start)-1):(int(end)])
+			#body.append(rev_comp_test(start,end,strand,f))
+		#header.append(">"+list[0]+"_"+gene_info[1])
+		#body.append(rev_comp_test(list,genome))
 
 	length=len(header)
 
-	return header,body,length
+	#return header,body,length
+	return length
 #currently quits after one iteration of for loop bc of return... how to fix?
 
 		#close .gff file
 	file.close()
 
 #write function to calc reverse complement if needed
-def rev_comp_test(list,genome):
+#def rev_comp_test(start,end,strand,genome):
 #not fixed if list[6] is made into a variable, or if moved above prev
 	#determine if strand is + or -, calculate reverse complement of - strand nt
-	if list[6] == "+":
-		return genome[(int(list[3])-1):(int(list[4])-1)]
-	else:
-		reverse_complement=rev_comp(genome[(int(list[3])-1):(int(list[4])-1)])
-		return reverse_complement
+	#if strand == "+":
+		#return genome[(int(start)-1):(int(end)]
+	#else:
+		#reverse_complement=rev_comp(genome[(int(start)-1):(int(end)])
+		#return reverse_complement
 
 #write function that calculates reverse complement
 def rev_comp(DNAseq):
@@ -84,10 +111,13 @@ def rev_comp(DNAseq):
 
 #define main function (connects all the modular pieces)
 def main():
-	header,body,length=gff_parse(args.fasta,args.gff)
-	for i in range(length):
-		print(header[i])
-		print(body[i])
+	genome=fasta_parse(args.fasta)
+	parse_gff()
+	#header,body,length=gff_parse(genome,args.gff)
+
+	#for i in range(length):
+		#print(header[i])
+		#print(body[i])
 
 #call command-line arguments
 args=get_args()
